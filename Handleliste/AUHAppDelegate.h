460ebb9036472fbfd8a63fa966694501470c35f4
AnnetteU <annette.undheim@gmail.com>
2014-01-10 13:40:28 +0100

    Initial project commit

---
+++ Handleliste/AUHAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AUHAppDelegate.h
+//  Handleliste
+//
+//  Created by Annette Undheim on 10/01/14.
+//  Copyright (c) 2014 Annette Undheim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AUHAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Handleliste/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Handleliste
+//
+//  Created by Annette Undheim on 10/01/14.
+//  Copyright (c) 2014 Annette Undheim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AUHAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AUHAppDelegate class]));
+    }
+}
